2adf011bf7c51b6714d973ca114d9438af475dc1
abdulalnakib <abdulalnakib10@gmail.com>
2024-03-10 21:22:46 +0600

    practice7 done

---
+++ practice7.js
@@ -0,0 +1,7 @@
+// Create an array of all phones and display the element of the array with while loop
+var phones = ['Oneplus', 'Xiaomi', 'Apple', 'Samsung', 'Vivo'];
+var i = 0;
+while (i < phones.length) {
+    console.log(phones[i]);
+    i++;
+}
