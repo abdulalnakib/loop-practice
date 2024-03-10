a31b46e61bacd09c7efeb246194f3bbb6b9d3ea8
abdulalnakib <abdulalnakib10@gmail.com>
2024-03-10 20:49:20 +0600

    practice6 done

---
+++ practice6.js
@@ -0,0 +1,7 @@
+// Declare all the topics in an array that u learnt last few days and put it as an output
+var topics = ['javascript', 'variables', 'numeric', 'boolean', 'string', 'and more'];
+
+for (var i = 0; i < topics.length; i++) {
+    var topic = topics[i];
+    console.log(topic)
+}
