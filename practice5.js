b524bb64be07517454e811a3c01dcf7a9bfe0302
abdulalnakib <abdulalnakib10@gmail.com>
2024-03-10 19:50:35 +0600

    practice5 done

---
+++ practice5.js
@@ -0,0 +1,17 @@
+// Difference between for and while loop
+
+// Display 1 to 10 in both loops
+
+// for loop
+
+for (var i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+// while loop
+
+var num1 = 1;
+while (num1 <= 10) {
+    console.log(num1);
+    num1++;
+}
